2e2e6c7f9f3f5ab3a0df8e95bc51d744f187c3e8
oluwatobi <oluwatobiajayi001@gmail.com>
2024-08-19 23:25:30 +0100

    modified:   app/Http/Controllers/ItemListController.php

--- app/Http/Controllers/ItemListController.php
+++ app/Http/Controllers/ItemListController.php
@@ -14,31 +14,35 @@ class ItemListController extends Controller
 
     public function store(Request $request){
         $validator = Validator::make($request->all(), [
-            'item_name' => 'required|min:3',
-            'quantity' => 'required',
-            'price' => 'required',
-            'total' => 'required'
+            'items.*.itemName' => 'required|string',
+            'items.*.Qty' => 'required|integer|min:1',
+            'items.*.Price' => 'required|numeric|min:0',
         ]);
-
-        if($validator->fails()){
+        
+        if ($validator->fails()) {
             return response()->json([
                 'status' => false,
                 'message' => 'Please fix errors',
                 'errors' => $validator->errors()
             ]);
         }
-
-        $item_list = new ItemList();
-        $item_list->item_name = $request->item_name;
-        $item_list->quantity = $request->quantity;
-        $item_list->price = $request->price;
-        $item_list->total = $request->total;
-        $item_list->save();
-
+        
+        $item_list_array = [];
+        
+        foreach ($request->item_name as $key => $item_name) {
+            $item_list = new ItemList();
+            $item_list->item_name = $item_name;
+            $item_list->quantity = $request->quantity[$key];
+            $item_list->price = $request->price[$key];
+            $item_list->total = $request->total[$key];
+            $item_list->save();
+            $item_list_array[] = $item_list;
+        }
+        
         return response()->json([
             'status' => true,
-            'message' => 'Item Added Successfully',
-            'data' => $item_list
+            'message' => 'Items Added Successfully',
+            'data' => $item_list_array
         ]);
     }
 }
